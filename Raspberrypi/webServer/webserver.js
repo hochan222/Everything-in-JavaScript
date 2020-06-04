1b1e6ac6012f477d77f30e5c67da1969fa7cdc90
egas <201502766@hufs.ac.kr>
2020-06-04 17:53:56 +0900

    [COMPLETE] socket.io

--- Raspberrypi/webServer/webserver.js
+++ Raspberrypi/webServer/webserver.js
@@ -1,6 +1,9 @@
 var http = require('http').createServer(handler);
-
 var fs = require('fs');
+var io = require('socket.io')(http);
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
+var pushButton = new Gpio(17, 'in', 'both');
 
 http.listen(8080);
 
@@ -15,3 +18,28 @@ function handler(req, res) {
 		return res.end();
 	});
 }
+
+io.sockets.on('connection', function (socket) {
+	var lightvalue = 0;
+	pushButton.watch(function (err, value) {
+		if (err) {
+			console.error('There was an error', err);
+			return;
+		}
+		lightvalue = value;
+		socket.emit('light', lightvalue);
+	});
+	socket.on('light', function (data) {
+		lightvalue = data;
+		if (lightvalue != LED.readSync()) {
+			LED.writeSync(lightvalue);
+		}
+	});
+});
+
+ProcessingInstruction.on('SIGINT', function () {
+	LED.writeSync(0);
+	LED.unexport();
+	pushButton.unexport();
+	process.exit();
+});
