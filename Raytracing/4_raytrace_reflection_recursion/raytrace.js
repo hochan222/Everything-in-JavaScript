211f56305ce5f9a5228ee50345427d2c2a92b97a
hochan <201502766@hufs.ac.kr>
2020-06-07 19:23:42 +0900

    [COMPELETE] raytrace_reflection_recursion

--- Raytracing/4_raytrace_reflection_recursion/raytrace.js
+++ Raytracing/4_raytrace_reflection_recursion/raytrace.js
@@ -1,28 +1,43 @@
+// global variables
+var light_location;
+var list_of_scene_objects = [];
 const WINDOW_WIDTH = 800;
 const WINDOW_HEIGHT = 600;
+
+///////////////////////////////////////////
 
 function setup() {
   createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
+
+///////////////////////////////////////////
 
 function draw() {
   // camera
   var camera_loc = createVector(0, 0, -100);
 
   // light source
-  var light_location = createVector(100, 100, -50);
+  light_location = createVector(100, 100, -50);
 
   // viewpoint ==> assume (0, 0, 0)
   var viewpoint_width = 40;
   var viewpoint_height = 30;
 
   // list of scene objects
-  var list_of_scene_objects = [];
-  list_of_scene_objects.push(new Sphere(0, 7.5, 30, 3, createVector(255, 0, 0)))
-  list_of_scene_objects.push(new Sphere(10, 3, 10, 5, createVector(0, 0, 255)))
-  list_of_scene_objects.push(new Sphere(-10, 3, 10, 5, createVector(255, 255, 0)))
-  list_of_scene_objects.push(new Sphere(0, -2, 0, 5, createVector(0, 255, 0)))
-  list_of_scene_objects.push(new Sphere(-1.5, 5.5, 0, 2, createVector(254, 1, 154)))
+  list_of_scene_objects.push(
+    new Sphere(0, 7.5, 30, 3, createVector(255, 0, 0))
+  );
+  list_of_scene_objects.push(new Sphere(10, 3, 10, 5, createVector(0, 0, 255)));
+  list_of_scene_objects.push(
+    new Sphere(-10, 3, 10, 5, createVector(255, 255, 0))
+  );
+  list_of_scene_objects.push(new Sphere(0, -2, 0, 5, createVector(0, 255, 0)));
+  list_of_scene_objects.push(
+    new Sphere(-1.5, 5.5, 0, 2, createVector(254, 1, 154))
+  );
+
+  // depth of rays
+  var max_depth = 3;
 
   for (var pixel_x = 0; pixel_x < WINDOW_WIDTH; pixel_x += 1) {
     for (var pixel_y = 0; pixel_y < WINDOW_HEIGHT; pixel_y += 1) {
@@ -37,77 +52,127 @@ function draw() {
       var ray_direction = p5.Vector.sub(current_position, camera_loc);
       ray_direction.normalize();
 
-      // nearest point
-      var distance_to_nearest_point = Infinity;
-      var nearest_intersection_point;
-      var nearest_normal;
-      var nearest_rgb;
-      var intersected = false;
+      // set off recursive ray
+      var [hit, rgb] = recursive_ray(camera_loc, ray_direction, max_depth);
 
-      // test all objects in scene
-      for (var obj of list_of_scene_objects) {
-
-        // check intersection with object
-        var [hit, intersection_point, normal, rgb] = obj.intersection(camera_loc, ray_direction)
-
-        if (hit) {
-          intersected = true;
-          // find nearest point
-          var distance_to_point = p5.Vector.mag( p5.Vector.sub(intersection_point, camera_loc))
-          if (distance_to_point < distance_to_nearest_point) {
-            distance_to_nearest_point = distance_to_point;
-            nearest_intersection_point = intersection_point;
-            nearest_normal = normal;
-            nearest_rgb = rgb;
-          }
-        }
-      } // list of objects
-
-      // colour if intersected
-      if (intersected) {
-        // vector to light source
-        var to_light_source = p5.Vector.sub(light_location, nearest_intersection_point);
-        to_light_source.normalize();
-
-        // vector reflected ray
-        var reflected_ray = p5.Vector.sub(
-          p5.Vector.mult(nearest_normal, 2 * p5.Vector.dot(to_light_source, nearest_normal)),
-          to_light_source
-        );
-        reflected_ray.normalize();
-
-        // cosine factor
-        var cos_with_lightsource = p5.Vector.dot(nearest_normal, to_light_source);
-        var cos_ray_to_reflection = p5.Vector.dot(
-          reflected_ray,
-          p5.Vector.mult(ray_direction, -1)
-        );
-
-        // clip cosine
-        cos_with_lightsource = constrain(cos_with_lightsource, 0, 1);
-        cos_ray_to_reflection = constrain(cos_ray_to_reflection, 0, 1);
-
-        // phong factor
-        // cos_with_lightsource = pow(cos_with_lightsource, 2);
-        // cos_ray_to_reflection = pow(cos_ray_to_reflection, 2);
-
-        // lighting factor
-        var lighting = cos_with_lightsource * cos_ray_to_reflection;
-
-        // update rgb with lighting factor
-        var nearest_rgb = p5.Vector.mult(nearest_rgb, lighting)
-
-        // point is 1 pixel dot
-        stroke(nearest_rgb.x, nearest_rgb.y, nearest_rgb.z);
+      if (hit) {
+        stroke(rgb.x, rgb.y, rgb.z);
         point(pixel_x, 600 - pixel_y);
       } else {
-        // background gradient
-        stroke(50, 50, 100 + ray_direction.y * 1000);
+        stroke(100, 100, 200 + ray_direction.y * 1000);
         point(pixel_x, 600 - pixel_y);
       }
     }
-  }
+  } // pixel loop
 }
+
+///////////////////////////////////////////////////////
+
+function recursive_ray(ray_source, ray_direction, depth) {
+  var colour_contribution = createVector(0, 0, 0);
+  var intersected = false;
+
+  // only continue if depth is more than 0
+  if (depth > 0) {
+    // nearest point
+    var distance_to_nearest_point = Infinity;
+    var nearest_intersection_point;
+    var nearest_normal;
+    var nearest_rgb;
+
+    // test all objects in scene
+    for (var obj of list_of_scene_objects) {
+      // check intersection with object
+      var [hit, intersection_point, normal, rgb] = obj.intersection(
+        ray_source,
+        ray_direction
+      );
+
+      if (hit) {
+        intersected = true;
+        // find nearest point
+        var distance_to_point = p5.Vector.mag(
+          p5.Vector.sub(intersection_point, ray_source)
+        );
+        if (distance_to_point < distance_to_nearest_point) {
+          distance_to_nearest_point = distance_to_point;
+          nearest_intersection_point = intersection_point;
+          nearest_normal = normal;
+          nearest_rgb = rgb;
+        }
+      }
+    } // list of objects
+
+    // if we hit object, we know the nearest object
+    if (intersected) {
+      // vector to light source
+      var to_light_source = p5.Vector.sub(
+        light_location,
+        nearest_intersection_point
+      );
+      to_light_source.normalize();
+
+      // vector reflected ray
+      var reflected_ray = p5.Vector.sub(
+        p5.Vector.mult(
+          nearest_normal,
+          2 * p5.Vector.dot(to_light_source, nearest_normal)
+        ),
+        to_light_source
+      );
+      reflected_ray.normalize();
+
+      // cosine factor
+      var cos_with_lightsource = p5.Vector.dot(nearest_normal, to_light_source);
+      var cos_ray_to_reflection = p5.Vector.dot(
+        reflected_ray,
+        p5.Vector.mult(ray_direction, -1)
+      );
+
+      // clip cosine
+      cos_with_lightsource = constrain(cos_with_lightsource, 0, 1);
+      cos_ray_to_reflection = constrain(cos_ray_to_reflection, 0, 1);
+
+      // phong factor
+      cos_with_lightsource = pow(cos_with_lightsource, 2);
+      cos_ray_to_reflection = pow(cos_ray_to_reflection, 2);
+
+      // lighting factor
+      var lighting = (0.1 + cos_with_lightsource * cos_ray_to_reflection) / 1.1;
+
+      // update rgb with lighting factor
+      var nearest_rgb = p5.Vector.mult(nearest_rgb, lighting);
+
+      // add to colour contribution
+      colour_contribution = p5.Vector.add(colour_contribution, nearest_rgb);
+
+      // bounced ray from new source, not from camera
+      var bounced_ray = p5.Vector.add(
+        p5.Vector.mult(
+          nearest_normal,
+          2 * p5.Vector.dot(p5.Vector.mult(ray_direction, -1), nearest_normal)
+        ),
+        ray_direction
+      );
+      bounced_ray.normalize();
+
+      // fire off recursive ray along bounced direction
+      var [hit2, rgb2] = recursive_ray(
+        nearest_intersection_point,
+        bounced_ray,
+        depth - 1
+      );
+      // scale colour down because of depth
+      rgb2 = p5.Vector.mult(rgb2, 0.5 * depth);
+      // contribution colourr
+      colour_contribution = p5.Vector.add(colour_contribution, rgb2);
+    }
+  }
+
+  return [intersected, colour_contribution];
+}
+
+///////////////////////////////////////////////////////
 
 class Sphere {
   constructor(x, y, z, r, col) {
@@ -146,6 +211,11 @@ class Sphere {
       var normal = p5.Vector.sub(intersection_point, this.sphere_loc);
       // normal = p5.Vector.add(normal, p5.Vector.random3D())
       normal.normalize();
+
+      // ensure t positive for forward ray direction
+      if (t < 0) {
+        delta = -1;
+      }
 
       return [delta >= 0, intersection_point, normal, this.rgb];
     }
